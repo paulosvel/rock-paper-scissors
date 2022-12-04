ab724472fc977b9ee184c675ec74d3176dae0548
paulosvel <paul2001vel@gmail.com>
2022-12-04 22:57:13 +0200

    quick fixes working perfect!

--- main.py
+++ main.py
@@ -4,22 +4,19 @@ cpuscore = 0
 cpupick = ["Rock","Paper","Scissors"]
 print("Rock Paper Scissors by Paulosvel")
 while (playerscore < 5 and cpuscore < 5):
+   cpuhand = random.choice(cpupick)
    while True:
     playerpick = input("Pick r for Rock, p for Paper, s for Scissors: ")
     if (playerpick == "r" or playerpick == "p" or playerpick == "s"):
      break
     else:
-     print("Invalid input pick again:")
-  
-   cpuhand = random.choice(cpupick)
-  
-   if (playerpick == "r" and cpupick == "Rock") or (playerpick == "p" and cpupick == "Paper") or (playerpick == "s" and cpupick == "Scissors"):
+     print("Invalid input pick again:") 
+   if (playerpick == "r" and cpuhand == "Rock") or (playerpick == "p" and cpuhand == "Paper") or (playerpick == "s" and cpuhand == "Scissors"):
     print("It's a tie try again!")
-   elif (playerpick == "r" and cpupick == "Scissors") or (playerpick == "s" and cpupick == "Paper") or (playerpick == "p" and cpupick == "Rock"):
+   elif (playerpick == "r" and cpuhand == "Scissors") or (playerpick == "s" and cpuhand == "Paper") or (playerpick == "p" and cpuhand == "Rock"):
     playerscore = playerscore+1
     print("You Won!",playerscore)
    else:
     cpuscore = cpuscore + 1
     print("You lost",cpuscore)  
 print("Your score: ",playerscore,"Cpu Score: ",cpuscore)
-
